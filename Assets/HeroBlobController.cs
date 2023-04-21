25377fa6a27a87098a5e15157195ee3b096637c5
Nosirrah01 <nosirrah01@gmail.com>
2023-04-21 15:38:27 -0400

    Consolidate movement logic and use single float variable to represent movement direction.

--- Assets/HeroBlobController.cs
+++ Assets/HeroBlobController.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class HeroBlobController : MonoBehaviour
 {
     public float speed = 5.0f;
-    private bool moveRight = false;
-    private bool moveLeft = false;
+    private float moveDirection = 0.0f;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -21,57 +20,36 @@ public class HeroBlobController : MonoBehaviour
         // Check if there is any touch input.
         if (Input.touchCount > 0)
         {
+            // Reset moveDirection
+            moveDirection = 0.0f;
+
             Touch touch = Input.GetTouch(0); // Get the first touch (assuming a single touch input).
 
-            // Check if the touch is on the right side of the screen.
-            if (touch.position.x > Screen.width / 2)
+            // Check if there is any touch input.
+            if (Input.touchCount > 0)
             {
-                // Set moveRight to true only if the touch is in the "Began" or "Stationary" phase.
-                moveRight = touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary;
-                moveLeft = false;
+                Touch touch = Input.GetTouch(0); // Get the first touch (assuming a single touch input).
+
+                // Check if the touch is in the "Began" or "Stationary" phase.
+                if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary)
+                {
+                    // Check if the touch is on the right side of the screen.
+                    if (touch.position.x > Screen.width / 2)
+                    {
+                        moveDirection = 1.0f; // Move right
+                    }
+                    else
+                    {
+                        moveDirection = -1.0f; // Move left
+                    }
+                }
             }
-            else
-            {
-                // If the touch is on the left side of the screen, set moveLeft to true and moveRight to false.
-                moveRight = false;
-                moveLeft = touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary;
-            }
-        }
-        else
-        {
-            // If there's no touch input, set moveRight and moveLeft to false.
-            moveRight = false;
-            moveLeft = false;
+
+            Move();
         }
 
-        // If moveRight is true, move the HeroBlob to the right.
-        if (moveRight)
+        void Move()
         {
-            MoveRight();
-        }
-        // If moveLeft is true, move the HeroBlob to the left.
-        else if (moveLeft)
-        {
-            MoveLeft();
-        }
-        else
-        {
-            StopMoving();
+            rb.velocity = new Vector2(moveDirection * speed, rb.velocity.y);
         }
     }
-
-    void MoveRight()
-    {
-        rb.velocity = new Vector2(speed, rb.velocity.y);
-    }
-
-    void MoveLeft()
-    {
-        rb.velocity = new Vector2(-speed, rb.velocity.y);
-    }
-
-    void StopMoving()
-    {
-        rb.velocity = new Vector2(0, rb.velocity.y);
-    }
-}
